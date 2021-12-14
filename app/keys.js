19ec368176f83aae59e193ad281c8c454c528ab3
Ashish <ashish@reapit.in>
2021-12-14 12:51:55 +0530

    v6

--- app/keys.js
+++ app/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    baseUrl: process.env.BASE_URL,
     mongoUsername: process.env.MONGO_USERNAME,
     mongoPassword: process.env.MONGO_PASSWORD,
     mongoUrl: process.env.MONGO_URL,
